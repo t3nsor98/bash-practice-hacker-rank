7a6c356a092a33c53916cefe9f10249dc552ca0a
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-02-22 00:20:35 +0530

    Remove personalizedecho.sh script

---
+++ solutions/comparingNumbers.sh
@@ -0,0 +1,16 @@
+read X
+read Y
+if (($X > $Y))
+then
+    echo "X is greater than Y"
+fi
+
+if ((($X == $Y)))
+then
+    echo "X is equal to Y"
+fi
+
+if (($X < $Y))
+then
+    echo "X is less than Y"
+fi
---
+++ solutions/loopingwithnumbers.sh
@@ -0,0 +1,5 @@
+for ((i = 1; i <= 50; i++))
+do
+    echo $i
+done
+# this will print numbers from 1 to 50
